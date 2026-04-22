47228533c388001d12dc0ba6a5eff3ec760dd514
RhysWheater <rhys.wheater@sophos.com>
2026-04-22 16:35:39 +0100

    Rename project to lazy-pr.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from pr-tui!")
+import subprocess
 
+def main():
+    print("Hello from lazy-pr!")
+    subprocess.call(["vim", "README.md"])
+    print("exited vim back into python")
 
 if __name__ == "__main__":
     main()
