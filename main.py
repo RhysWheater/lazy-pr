fa10de4fedff63255cf31faee469c91040279300
RhysWheater <rhys.wheater@sophos.com>
2026-04-22 17:01:44 +0100

    Add a command that gets a list of all open PRs on the requested repo.

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-import subprocess
+import os
+import typer
+from github import Github
+from github.Auth import Token
 
-def main():
-    print("Hello from lazy-pr!")
-    subprocess.call(["vim", "README.md"])
-    print("exited vim back into python")
+
+app = typer.Typer()
+
+
+def get_github():
+    token = os.environ.get("GITHUB_TOKEN")
+    if not token:
+        typer.echo("Missing $GITHUB_TOKEN", err=True)
+        raise typer.Exit(1)
+    return Github(auth=Token(token))
+
+
+@app.command()
+def list_prs(repo: str):
+    g = get_github()
+    for pr in g.get_repo(repo).get_pulls(state="open"):
+        typer.echo(f"#{pr.number} {pr.title} - {pr.user.login}")
+
+
+@app.command()
+def say_hello():
+    typer.echo('hello')
+
 
 if __name__ == "__main__":
-    main()
+    app()
